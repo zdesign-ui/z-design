43a34d5d9cb841c83f97dc5ec8b312c3feef8ddd
ming zhong <ming.zhong@corp.matrx.team>
2021-02-26 13:52:08 +0800

    add nprogress lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app'
+import Router from 'next/router'
 import Layout from '../components/layout'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
 import '../styles/globals.scss'
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }: AppProps) {
   return (
