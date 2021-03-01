687815b2f5f7fcc01d90b2dbbbf52b5055ca4df2
mizhon <mizhon@live.cn>
2021-03-01 21:46:40 +0800

    update codes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 // import App from "next/app";
+import dynamic from 'next/dynamic'
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Layout from '../components/layout'
 import '../styles/globals.scss'
+import 'nprogress/nprogress.css'
+
+const TopProgressBar = dynamic(
+  () => {
+    return import('../components/TopProgressBar')
+  },
+  { ssr: false }
+)
 
 function App({ Component, pageProps }: AppProps) {
   return (
     // <Layout>
+    <>
+      <TopProgressBar />
       <Component {...pageProps} />
+    </>  
     // </Layout>
   )
 }
